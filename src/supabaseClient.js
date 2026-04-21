7df8a3ece1fe088f60e4af18b27474b714a9f654
AuraflowTech <jaroslavhorejsi95@gmail.com>
2026-04-21 13:02:17 +0200

    AUTH fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://fssbxucvfyaxuchayzox.supabase.co'
 const supabaseAnonKey = 'sb_publishable_uWrUJxw4nFQpkO7aduvkFg_YNTbOinI'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      persistSession: true, // Tohle musí být true!
+      autoRefreshToken: true,
+    }
+  })
